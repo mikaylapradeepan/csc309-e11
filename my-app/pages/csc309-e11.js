b45f34c8661e20787d932a24cef9ab1f31c40074
MIKAYLA PRADEEPAN <mikaylapradeepan@gmail.com>
2024-11-28 14:44:47 -0500

    f

--- my-app/pages/csc309-e11.js
+++ my-app/pages/csc309-e11.js
@@ -1,7 +1,8 @@
 export default function AutoTester() {
     return (
       <div id="auto-tester">
-        Hi. I am pradee23, and I have learned how to develop a full-stack web app and deploy it to the internet.
+        Hi. I am pradee23, and I have learned how to develop a full-stack web app
+        and deploy it!
       </div>
     )
   }
